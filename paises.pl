de4e201bcc4d1637c073297b4178f65467ce9212
guilhermedesales <128305065+guilhermedesales@users.noreply.github.com>
2024-10-26 18:17:05 -0300

    .

---
+++ paises.pl
@@ -0,0 +1,45 @@
+% Fatos: pais(clima, area, lingua, motivação, pais).
+
+% quente, urbana
+pais(quente, urbana, ingles, educacao, 'Emirados Arabes Unidos').
+pais(quente, urbana, ingles, trabalho, 'Singapura').
+pais(quente, urbana, ingles, 'melhoria de vida', 'Filipinas').
+pais(quente, urbana, espanhol, educacao, 'Espanha').
+pais(quente, urbana, espanhol, trabalho, 'Mexico').
+pais(quente, urbana, espanhol, 'melhoria de vida', 'Colombia').
+pais(quente, urbana, portugues, educacao, 'Portugal').
+pais(quente, urbana, portugues, trabalho, 'Angola').
+pais(quente, urbana, portugues, 'melhoria de vida', 'Cabo Verde').
+
+% quente, rural
+pais(quente, rural, ingles, educacao, 'Quenia').
+pais(quente, rural, ingles, trabalho, 'Botswana').
+pais(quente, rural, ingles, 'melhoria de vida', 'Jamaica').
+pais(quente, rural, espanhol, educacao, 'Paraguai').
+pais(quente, rural, espanhol, trabalho, 'Guatemala').
+pais(quente, rural, espanhol, 'melhoria de vida', 'Honduras').
+pais(quente, rural, portugues, educacao, 'Moçambique').
+pais(quente, rural, portugues, trabalho, 'Guine-Bissau').
+pais(quente, rural, portugues, 'melhoria de vida', 'Timor-Leste').
+
+% frio, urbana
+pais(frio, urbana, ingles, educacao, 'Canada').
+pais(frio, urbana, ingles, trabalho, 'Reino Unido').
+pais(frio, urbana, ingles, 'melhoria de vida', 'Estados Unidos').
+pais(frio, urbana, espanhol, educacao, 'Argentina').
+pais(frio, urbana, espanhol, trabalho, 'Chile').
+pais(frio, urbana, espanhol, 'melhoria de vida', 'Uruguai').
+pais(frio, urbana, portugues, educacao, 'Suica').
+pais(frio, urbana, portugues, trabalho, 'Luxemburgo').
+pais(frio, urbana, portugues, 'melhoria de vida', 'Belgica').
+
+% frio, rural
+pais(frio, rural, ingles, educacao, 'Nova Zelandia').
+pais(frio, rural, ingles, trabalho, 'Islandia').
+pais(frio, rural, ingles, 'melhoria de vida', 'Noruega').
+pais(frio, rural, espanhol, educacao, 'Bolivia').
+pais(frio, rural, espanhol, trabalho, 'Peru').
+pais(frio, rural, espanhol, 'melhoria de vida', 'Andorra').
+pais(frio, rural, portugues, educacao, 'Austria').
+pais(frio, rural, portugues, trabalho, 'Eslovenia').
+pais(frio, rural, portugues, 'melhoria de vida', 'Letonia').
---
+++ python_consult.py
@@ -0,0 +1,29 @@
+from pyswip import Prolog # pip3 install pyswip Collecting pyswip
+
+prolog = Prolog()
+prolog.consult('paises.pl')
+
+def consulta_pais(clima, area, lingua, motivacao):
+    consulta = f"pais({clima}, {area}, {lingua}, {motivacao}, Pais)"
+    resultado = list(prolog.query(consulta))
+    
+    if resultado:
+        pais = resultado[0]['Pais']
+        print(f'O país sugerido é: {pais}')
+    else:
+        print('algum erro de digitação')
+
+def main():
+
+    clima = input('Qual o clima de preferência? (quente, frio): ')
+
+    area = input('Qual a área de preferência? (urbana, rural): ')
+
+    lingua = input('Qual a língua nativa de preferência? (inglês, espanhol, português): ')
+    
+    motivacao = input('Qual a sua motivação de mudança? (educação, trabalho, "melhoria de vida"): ')
+    
+    consulta_pais(clima, area, lingua, motivacao)
+
+if __name__ == '__main__':
+    main()
